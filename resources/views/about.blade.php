df5b90bb5bba13487fcd9bd4d194c0c0823ad297
Agus Suryadiyatmika <suryadiyatmika31@gmail.com>
2025-06-01 20:25:27 +0800

    initial commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sistem Pengajuan Peminjaman Inventaris</title>
+</head>
+<body>
+    <header>
+        <h1>Ini adalah halaman About</h1>
+    </header>
+    <main>
+        {{$products[0]->name}}
+        <br>
+        {{$products[0]->description}}
+    </main>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/home', function () {
+    return view('welcome');
+});
+
+// Route::get('/about', function() {
+//     return view('about');
+// });
+
+Route::get('/about', [HomeController::class, 'index']);
